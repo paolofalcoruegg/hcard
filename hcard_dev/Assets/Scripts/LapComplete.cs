2ec98dfa6ab27a601aed806c546a1db665abe9e9
CharisGeo <charisgeorgiou.cy@gmail.com>
2019-03-15 17:19:40 +0200

    Added transition for LowPoly Scene to Main Menu

--- hcard_dev/Assets/Scripts/LapComplete.cs
+++ hcard_dev/Assets/Scripts/LapComplete.cs
@@ -8,42 +8,10 @@ public class LapComplete : MonoBehaviour
 
     public GameObject LapCompleteTrig;
     public GameObject StartLapTrig;
-
-    public GameObject MinuteDisplay;
-    public GameObject SecondDisplay;
-    public GameObject MilliDisplay;
-
-    public GameObject LapTimeBox;
-
     public GameManager gameManager;
 
     void OnTriggerEnter()
     {
-
-        if (LapTimeManager.SecondCount <= 9)
-        {
-            SecondDisplay.GetComponent<Text>().text = "0" + LapTimeManager.SecondCount + ".";
-        }
-        else
-        {
-            SecondDisplay.GetComponent<Text>().text = "" + LapTimeManager.SecondCount + ".";
-        }
-
-        if (LapTimeManager.MinuteCount <= 9)
-        {
-            MinuteDisplay.GetComponent<Text>().text = "0" + LapTimeManager.MinuteCount + ".";
-        }
-        else
-        {
-            MinuteDisplay.GetComponent<Text>().text = "" + LapTimeManager.MinuteCount + ".";
-        }
-
-        MilliDisplay.GetComponent<Text>().text = "" + LapTimeManager.MilliCount;
-
-        LapTimeManager.MinuteCount = 0;
-        LapTimeManager.SecondCount = 0;
-        LapTimeManager.MilliCount = 0;
-
         StartLapTrig.SetActive(true);
         LapCompleteTrig.SetActive(false);
 
---
+++ hcard_dev/Assets/Scripts/LowPolyLevelComplete.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LowPolyLevelComplete : MonoBehaviour
+{
+    public void LoadNextLevel() => SceneManager.LoadScene(sceneBuildIndex: SceneManager.GetActiveScene().buildIndex - 2);
+}
