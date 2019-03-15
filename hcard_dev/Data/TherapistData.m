f636e8dd51da42fc180bd68fea781988ebf89388
Alissa1998 <alissaparmenter@outlook.com>
2019-03-15 11:56:22 +0000

    Matlab stuff

--- hcard_dev/Data/TherapistData.m
+++ hcard_dev/Data/TherapistData.m
@@ -1,21 +1,34 @@
 
-data = csvread('user_data.txt');
+user_data = csvread('user_data.txt');
+path_data = csvread('Path data - Sheet1.csv');
 
-time = data(1:end,1);
+timeuser = user_data(1:end,1);
+alpha = user_data(1:end,2);
+beta = user_data(1:end,3);
+yuser = user_data(1:end,4);
+xuser = user_data(1:end,5);
 
-alpha = data(1:end,2);
+ypath = path_data(1:end,3);
+xpath = path_data(1:end,2);
+timepath = path_data(1:end,1);
 
-beta = data(1:end,3);
+fitpath = fit(xpath,ypath,'linearinterp');
+
+x = 0:0.1:650;
+figure
+plot(x,fitpath(x));
+
 
 figure
-subplot(2,1,1);
-plot(time,alpha)
-subplot(2,1,2);
-plot(time,beta)
+subplot(3,1,1);
+plot(xuser,yuser,xpath,ypath)
+subplot(3,1,2);
+plot(timeuser,alpha)
+subplot(3,1,3);
+plot(timeuser,beta)
 
-max_angle_left = max(alpha);
-max_angle_right = min(alpha);
+max_angle_right = max(alpha);
+max_angle_left = min(alpha);
 max_angle_forward = max(beta);
 max_angle_backward = min(beta);
-
 
